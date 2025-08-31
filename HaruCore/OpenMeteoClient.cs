a5ad53985eae3f8fd4ab1535a9b806f0a084e053
FriendsNone <wizzmurr@outlook.com>
2025-09-01 00:10:29 +0800

    first ideration of open-meteo stuff

---
+++ HaruCore/OpenMeteoClient.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using NodaTime;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace HaruCore
+{
+    public class OpenMeteoClient
+    {
+        public void SearchLocation(string query, Action<List<LocationResult>, Exception> callback, int count = 5)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                if (callback != null) callback(null, new ArgumentException("query empty"));
+                return;
+            }
+
+            string url = string.Format(
+                "http://geocoding-api.open-meteo.com/v1/search?name={0}&count={1}&language=en",
+                Uri.EscapeDataString(query),
+                count);
+
+            WebClient wc = new WebClient();
+            wc.DownloadStringCompleted += (s, e) =>
+            {
+                if (e.Error != null)
+                {
+                    if (callback != null) callback(null, e.Error);
+                    return;
+                }
+                try
+                {
+                    GeocodingResponse geo = JsonConvert.DeserializeObject<GeocodingResponse>(e.Result);
+                    if (callback != null) callback(geo != null ? geo.Results : null, null);
+                }
+                catch (Exception ex)
+                {
+                    if (callback != null) callback(null, ex);
+                }
+            };
+            wc.DownloadStringAsync(new Uri(url));
+        }
+
+        public void GetForecast(double latitude, double longitude, Action<ForecastResponse, Exception> callback)
+        {
+            string lat = latitude.ToString(CultureInfo.InvariantCulture);
+            string lon = longitude.ToString(CultureInfo.InvariantCulture);
+            string tz = DateTimeZoneProviders.Tzdb.GetSystemDefault().Id;
+            string url = string.Format(
+                "http://api.open-meteo.com/v1/forecast?latitude={0}&longitude={1}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone={2}",
+                lat,
+                lon,
+                tz);
+
+            WebClient wc = new WebClient();
+            wc.DownloadStringCompleted += (s, e) =>
+            {
+                if (e.Error != null)
+                {
+                    if (callback != null) callback(null, e.Error);
+                    return;
+                }
+                try
+                {
+                    ForecastResponse forecast = JsonConvert.DeserializeObject<ForecastResponse>(e.Result);
+                    if (callback != null) callback(forecast, null);
+                }
+                catch (Exception ex)
+                {
+                    if (callback != null) callback(null, ex);
+                }
+            };
+            wc.DownloadStringAsync(new Uri(url));
+        }
+    }
+}
---
+++ HaruCore/OpenMeteoModel.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HaruCore
+{
+    public class GeocodingResponse
+    {
+        [JsonProperty("results")]
+        public List<LocationResult> Results { get; set; }
+    }
+
+    public class LocationResult
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+        [JsonProperty("country_code")]
+        public string CountryCode { get; set; }
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("admin1")]
+        public string Admin1 { get; set; }
+        [JsonProperty("admin2")]
+        public string Admin2 { get; set; }
+        [JsonProperty("admin3")]
+        public string Admin3 { get; set; }
+        [JsonProperty("admin4")]
+        public string Admin4 { get; set; }
+
+        public string LocationName
+        {
+            get
+            {
+                return string.Format("{0}, {1}, {2}",
+                    Name,
+                    Admin1,
+                    Country);
+            }
+        }
+
+        public string LocationCoordinates
+        {
+            get
+            {
+                return string.Format("{0}, {1}",
+                    Latitude,
+                    Longitude);
+            }
+        }
+    }
+
+    public class ForecastResponse
+    {
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("current")]
+        public CurrentWeather CurrentWeather { get; set; }
+
+        //[JsonProperty("hourly")]
+        //public Hourly? Hourly { get; set; }
+
+        //[JsonProperty("hourly_units")]
+        //public Daily? Daily { get; set; }
+    }
+
+    public class CurrentWeather
+    {
+        [JsonProperty("time")]
+        public string Time { get; set; }
+        [JsonProperty("temperature_2m")]
+        public double Temperature { get; set; }
+        [JsonProperty("relative_humidity_2m")]
+        public double RelativeHumidity { get; set; }
+        [JsonProperty("apparent_temperature")]
+        public double ApparentTemperature { get; set; }
+        [JsonProperty("precipitation")]
+        public double Precipitation { get; set; }
+        [JsonProperty("rain")]
+        public double Rain { get; set; }
+        [JsonProperty("showers")]
+        public double Showers { get; set; }
+        [JsonProperty("snowfall")]
+        public double Snowfall { get; set; }
+        [JsonProperty("weather_code")]
+        public int WeatherCode { get; set; }
+        [JsonProperty("cloud_cover")]
+        public double CloudCover { get; set; }
+        [JsonProperty("pressure_msl")]
+        public double Pressure { get; set; }
+        [JsonProperty("surface_pressure")]
+        public double SurfacePressure { get; set; }
+        [JsonProperty("wind_speed_10m")]
+        public double WindSpeed { get; set; }
+        [JsonProperty("wind_direction_10m")]
+        public double WindDirection { get; set; }
+        [JsonProperty("wind_gusts_10m")]
+        public double WindGusts { get; set; }
+    }
+}
